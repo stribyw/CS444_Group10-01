f324e014d2377d8afd9fa4037993046f16c188d8
stribyw <stribyw@oregonstate.edu>
2017-04-19 18:50:10 -0700

    Add files via upload

---
+++ producer_consumer.c
@@ -0,0 +1,54 @@
+/*
+ *Shannon Ernst, Tanner Cecchetti, W Keith Striby Jr
+ *Assignment 1, The Producer-Consumer Problem
+ *
+ *
+ *
+ */
+
+#include <stdio.h>
+#include <pthread.h>
+
+pthread_t prod_con;
+//pthread_mutex_t lock;
+pthread_cond_t con_cond, prod_cond;
+int buffer = 32;
+
+void* producer(void* ptr){
+
+    int i;
+
+    while(true){
+        pthread_mutex_lock(&prod_con);      //protect buffer
+        while(buffer != 0)
+            pthread_cond_wait(&prod_cond, &prod_con);
+        buffer++;
+        pthread_cond_signal(con_cond);
+        pthread_mutex_unlock(&prod_con);
+    }
+
+}
+
+
+int main(int argc, char* argv[]){
+    
+    pthread_t prod, con;
+    num_threads = argv[1];
+
+    /*
+     *initialize mutex and condition variables
+     *
+     */
+    pthread_mutex_init(&the_mutex, NULL);
+    pthread_cond_init(&con_cond, NULL);         //initialize consumer condition
+    pthread_cond_init(&prod_con, NULL);         //initialize producer condition
+
+    //create threads
+    pthread_create(&con, NULL, consumer, NULL);
+    pthread_create(&prod, NULL, producer, NULL);
+
+    //wait for other thread to finish
+    pthread_join(&con, NULL);
+    pthread_join(&prod, NULL);
+
+}
