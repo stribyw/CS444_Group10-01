3c1fc5ceaf5083dadc8bc2d271f1c9480d293b17
Tanner <cecchett@oregonstate.edu>
2017-04-21 01:12:05 -0700

    Change changelog table formatting

--- util/commit-history-to-tex.sh
+++ util/commit-history-to-tex.sh
@@ -8,6 +8,6 @@ origin=`git config remote.origin.url`
 base=`dirname "$origin"`/`basename "$origin" .git`
 
 # Output LaTeX table in reverse chronological order
-echo "\\begin{tabular}{p{1.5cm} p{4cm} p{3cm} p{8cm}}\\textbf{Commit} & \\textbf{Date} & \\textbf{Author} & \\textbf{Description}\\\\\\hline"
+echo "\\begin{tabular}{ | p{1.5cm} | p{4cm} | p{3cm} | p{8cm} | }\\hline\\textbf{Commit} & \\textbf{Date} & \\textbf{Author} & \\textbf{Description}\\\\\\hline"
 git log --pretty=format:"\\href{$base/commit/%H}{%h} & \\detokenize{%cd} & \\detokenize{%an} & \\detokenize{%s}\\\\\\hline" --date=format:'%d %B %Y %I:%M %p'
 echo "\\end{tabular}"
