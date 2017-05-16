2a7ee92705a3affac2df6b3d15975e5d5fc26021
Tanner <cecchett@oregonstate.edu>
2017-05-16 10:06:24 -0700

    Indent with 8 spaces

--- synchro3/Categorical_MutEx.c
+++ synchro3/Categorical_MutEx.c
@@ -29,8 +29,8 @@ int search_count = 0;
 * Structure for singly linked list.
 */
 struct node {
-	int val;
-	struct node* next;
+        int val;
+        struct node* next;
 };
 
 /*
@@ -38,19 +38,19 @@ struct node {
 */
 void push()
 {
-	struct node* new_node = malloc(sizeof(struct node));
-	new_node->val = rand() % 100 + 1;
-	new_node->next = NULL;
+        struct node* new_node = malloc(sizeof(struct node));
+        new_node->val = rand() % 100 + 1;
+        new_node->next = NULL;
 
-	if (head == NULL) {
-		head = new_node;
-	} else {
-		struct node* current = head;
-		while (current->next != NULL){
-			current = current->next;
-		}
-		current->next = new_node;
-	}
+        if (head == NULL) {
+                head = new_node;
+        } else {
+                struct node* current = head;
+                while (current->next != NULL){
+                        current = current->next;
+                }
+                current->next = new_node;
+        }
 }
 
 /*
@@ -58,13 +58,13 @@ void push()
 */
 void pop()
 {
-	struct node* temp;
+        struct node* temp;
 
-	if (head != NULL) {
-		temp = head;
-		head = head->next;
-		free(temp);
-	}
+        if (head != NULL) {
+                temp = head;
+                head = head->next;
+                free(temp);
+        }
 }
 
 /*
@@ -72,12 +72,12 @@ void pop()
 */
 void print_list()
 {
-	struct node* current = head;
-	while (current != NULL) {
-		printf(" %d ", current->val);
-		current = current->next;
-	}
-	printf("\n");
+        struct node* current = head;
+        while (current != NULL) {
+                printf(" %d ", current->val);
+                current = current->next;
+        }
+        printf("\n");
 }
 
 /*
@@ -85,20 +85,20 @@ void print_list()
 */
 void* delete(void* ptr)
 {
-	pthread_mutex_lock(&insert_lock);
-	pthread_mutex_lock(&delete_lock);
+        pthread_mutex_lock(&insert_lock);
+        pthread_mutex_lock(&delete_lock);
 
-	printf("deleting start\n");
+        printf("deleting start\n");
 
-	pop();
-	print_list();
+        pop();
+        print_list();
 
-	printf("deleting finish\n");
+        printf("deleting finish\n");
 
-	pthread_mutex_unlock(&insert_lock);
-	pthread_mutex_unlock(&delete_lock);
+        pthread_mutex_unlock(&insert_lock);
+        pthread_mutex_unlock(&delete_lock);
 
-	pthread_exit(NULL);
+        pthread_exit(NULL);
 }
 
 /*
@@ -106,25 +106,25 @@ void* delete(void* ptr)
 */
 void* search(void* ptr)
 {
-	pthread_mutex_lock(&search_count_lock);
-	search_count++;
-	printf("searches in progress: %d\n", search_count);
-	pthread_mutex_unlock(&search_count_lock);
+        pthread_mutex_lock(&search_count_lock);
+        search_count++;
+        printf("searches in progress: %d\n", search_count);
+        pthread_mutex_unlock(&search_count_lock);
 
-	struct node* current = head;
-	while (current != NULL) {
-		pthread_mutex_lock(&delete_lock);
-		current = current->next;
-		printf("looking at current\n");
-		pthread_mutex_unlock(&delete_lock);
-	}
+        struct node* current = head;
+        while (current != NULL) {
+                pthread_mutex_lock(&delete_lock);
+                current = current->next;
+                printf("looking at current\n");
+                pthread_mutex_unlock(&delete_lock);
+        }
 
-	pthread_mutex_lock(&search_count_lock);
-	search_count--;
-	printf("finished search: %d\n", search_count);
-	pthread_mutex_unlock(&search_count_lock);
+        pthread_mutex_lock(&search_count_lock);
+        search_count--;
+        printf("finished search: %d\n", search_count);
+        pthread_mutex_unlock(&search_count_lock);
 
-	pthread_exit(NULL);
+        pthread_exit(NULL);
 }
 
 /*
@@ -132,36 +132,36 @@ void* search(void* ptr)
 */
 void* insert(void* ptr)
 {
-	pthread_mutex_lock(&insert_lock);
+        pthread_mutex_lock(&insert_lock);
 
-	printf("insert start\n");
+        printf("insert start\n");
 
-	push();
-	print_list();
+        push();
+        print_list();
 
-	printf("insert finish\n");
+        printf("insert finish\n");
 
-	pthread_mutex_unlock(&insert_lock);
+        pthread_mutex_unlock(&insert_lock);
 
-	pthread_exit(NULL);
+        pthread_exit(NULL);
 }
 
 int main()
 {
-	int choice = 0;
-	time_t t;
+        int choice = 0;
+        time_t t;
 
-	srand ((unsigned) time(&t));
-	pthread_t thread;
+        srand ((unsigned) time(&t));
+        pthread_t thread;
 
-	while (1) {
-		choice = rand () % 3 + 1;
-		if (choice == 1){
-			pthread_create(&thread, NULL, search, NULL);
-		} else if (choice == 2) {
-			pthread_create(&thread, NULL, insert, NULL);
-		} else if (choice == 3) {
-			pthread_create(&thread, NULL, delete, NULL);
-		}
-	}
+        while (1) {
+                choice = rand () % 3 + 1;
+                if (choice == 1){
+                        pthread_create(&thread, NULL, search, NULL);
+                } else if (choice == 2) {
+                        pthread_create(&thread, NULL, insert, NULL);
+                } else if (choice == 3) {
+                        pthread_create(&thread, NULL, delete, NULL);
+                }
+        }
 }
